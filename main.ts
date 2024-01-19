6fe8134606330d5017119aa43f7cdab4d083d455
jnavas98 <j.navasdam@outlook.es>
2024-01-19 10:06:47 +0100

    ejemplo basico rest

---
+++ main.ts
@@ -0,0 +1,22 @@
+// @deno-types="npm:@types/express@4"
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+
+
+const MONGO_URL = Deno.env.get("MONGO_URL");
+
+if (!MONGO_URL) {
+  console.log("No mongo URL found");
+  Deno.exit(1);
+}
+
+await mongoose.connect(MONGO_URL);
+const app = express();
+app.use(express.json());
+app
+  .get("/", async (req: Request, res: Response) => {res.send("Funciona todo correctamente")})
+  
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
---
+++ resolvers/ejemplo.ts
@@ -0,0 +1,2 @@
+// @deno-types="npm:@types/express@4"
+import { Request, Response } from "express";
